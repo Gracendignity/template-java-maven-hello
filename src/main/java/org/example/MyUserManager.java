19b7cf4c2d60ae192113147a6cd233948ac203ec
Gracendignity vanial <hjx2455745979@petalmail.com>
2023-07-12 07:37:25 +0000

    1

--- src/main/java/org/example/MyUserManager.java
+++ src/main/java/org/example/MyUserManager.java
@@ -32,34 +32,33 @@ public class MyUserManager {
      * @return
      */
     public boolean login(String username, String password) {
+        Scanner scanner = new Scanner(System.in);
+        String Input="";
+
         try (Connection connection = DriverManager.getConnection(DB_URL);
              PreparedStatement statement = connection.prepareStatement("SELECT * FROM Users WHERE username = ?")) {
             statement.setString(1, username);
             ResultSet resultSet = statement.executeQuery();
-            
-            Scanner scanner = new Scanner(System.in);
-            
-            String Input="";
 
-            if (resultSet.next()) {
-                String storedPassword = resultSet.getString("password");
-                if (password.equals(storedPassword)) {
-                    System.out.println("Login successful!");
-                    return true;
+                if (resultSet.next()) {
+                    String storedPassword = resultSet.getString("password");
+                    if (password.equals(storedPassword)) {
+                        System.out.println("Login successful!");
+                        return true;
+                    } else {
+                        System.out.println("Incorrect password.");
+                        while(Input.equals(storedPassword)){
+                        System.out.println("请重新输入密码:");
+                        Input = scanner.nextLine();
+                        }
+                    }
                 } else {
-                    System.out.println("Incorrect password.");
-                    while(Input.equals(storedPassword)){
-                    System.out.println("请重新输入密码:");
-                    Input = scanner.nextLine();
+                    System.out.println("Username does not exist.");
+                    while(Input.equals(username)){
+                        System.out.println("请重新输入用户名:");
+                        Input = scanner.nextLine();
                     }
                 }
-            } else {
-                System.out.println("Username does not exist.");
-                while(Input.equals(username)){
-                    System.out.println("请重新输入用户名:");
-                    Input = scanner.nextLine();
-                }
-            }
         } catch (SQLException e) {
             System.out.println("Failed to login: " + e.getMessage());
         }
