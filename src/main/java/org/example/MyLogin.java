fcff2203554d5d92f2170cb07329c0008a3bdeac
Gracendignity vanial <hjx2455745979@petalmail.com>
2023-07-13 02:36:17 +0000

    1

--- src/main/java/org/example/MyLogin.java
+++ src/main/java/org/example/MyLogin.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MyLogin implements MyAction {
 
@@ -17,11 +19,17 @@ public class MyLogin implements MyAction {
     public String getActionName() {
         return MyLogin.ACTION_NAME;
     }
-
+     String input="";
     @Override
     public void run(String[] args) {
+
         System.out.println("欢迎进入登录界面!");
         
+        List<MyAction> list = new ArrayList<MyAction>();
+
+        MyPasswordManager passWord=new MyPasswordManager(scanner);
+        list.add(passWord);
+
         while(true) {
             System.out.print("请输入用户名:");
             String username = this.scanner.nextLine();
@@ -31,8 +39,21 @@ public class MyLogin implements MyAction {
 
             boolean success = this.userManager.login(username, password);
             if(success){
-                System.out.println("登录成功,返回上一级");
-                break;
+                System.out.println("登录成功,请输入指令：密码管理:passWord,顾客管理:customer,商品管理:product,q 退出");
+                input = this.scanner.nextLine();
+
+                if (input.equals("q")) {
+                    break; 
+                }
+
+            String actionName = null;
+            for(MyAction oneAction: list) {
+                actionName = oneAction.getActionName();
+
+                if (input.equalsIgnoreCase(actionName)) {
+                    oneAction.run(null);
+                }
+            }
             }
         }
         System.out.println("你已退出登录界面，回到管理员页面!");
--- src/main/java/org/example/MyPasswordManager.java
+++ src/main/java/org/example/MyPasswordManager.java
@@ -1,5 +1,102 @@
 package org.example;
 
-public class MyPasswordManager {
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class MyPasswordManager implements MyAction {
+
+    private static final String DB_URL = "jdbc:sqlite:users.db";
+
+    Scanner input = new Scanner(System.in);
+
+    private static final String ACTION_NAME = "passWord";
+
+    private Scanner scanner = null;
+
+    public MyPasswordManager(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    
+    @Override
+    public String getActionName() {
+        return MyPasswordManager.ACTION_NAME;
+    }
+
+    @Override
+    public void run(String[] args) {
+        System.out.println("欢迎进入密码管理界面!");
+        System.out.println("修改密码：change,重置密码:reset");
+        String Input = input.nextLine();
+        switch(Input){
+            case "change": 
+                         changePassword();
+                         break;
+            case "reset":
+                          resetPassword();
+                          break;
+
+        }
+    }
+    public void changePassword(){
+        while(true) {
+            
+            System.out.print("请输入用户名:");
+            String username = this.scanner.nextLine();
+
+            System.out.print("请输入旧密码:");
+            String password = this.scanner.nextLine();
+
+            System.out.print("请输入新密码:");
+            String newPassword = this.scanner.nextLine();
+            
+            try (Connection connection = DriverManager.getConnection(DB_URL);
+            PreparedStatement statement = connection.prepareStatement("UPDATE Users SET password = ? WHERE username = ?")) {
+           statement.setString(1, newPassword);
+           statement.setString(2, username);
+           int rowsUpdated = statement.executeUpdate();
+           if (rowsUpdated > 0) {
+               System.out.println("User information updated successfully!");
+           } else {
+               System.out.println("User not found!");
+           }
+       } catch(SQLException e) {
+           System.out.println("Error updating user information: " + e.getMessage());
+       }
+       System.out.println("修改密码成功!");
+       break;
+        }
+    }
+    public void resetPassword(){
+        while(true) {
+
+            System.out.print("请输入用户名:");
+            String username = this.scanner.nextLine();
+
+            try (Connection connection = DriverManager.getConnection(DB_URL);
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM Users WHERE username = ?")) {
+            statement.setString(1, username);
+            int rowsDeleted = statement.executeUpdate();
+        if (rowsDeleted > 0) {
+            System.out.println("User deleted successfully!");
+
+            System.out.print("请输入用户名:");
+            username = this.scanner.nextLine();
+
+            System.out.print("请输入重置后的密码:");
+            String password = this.scanner.nextLine();
+
+        } else {
+            System.out.println("User not found!");
+        }
+    } catch(SQLException e) {
+        System.out.println("Error deleting user: " + e.getMessage());
+    }
+      System.out.println("重置密码成功!");
+      break;
+        } 
+    }
     
 }
