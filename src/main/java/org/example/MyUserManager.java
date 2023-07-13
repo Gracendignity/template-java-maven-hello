706cb928da2042ce390ab4a88057c23ace3205a9
Gracendignity vanial <hjx2455745979@petalmail.com>
2023-07-13 07:35:53 +0000

    1

--- src/main/java/org/example/MyUserManager.java
+++ src/main/java/org/example/MyUserManager.java
@@ -29,7 +29,7 @@ public class MyUserManager {
     /**
      * @param username
      * @param password
-     *  @param userStatus
+     * @param userStatus
      * @return
      */
     public boolean login(String username, String password,String userStatus) {
